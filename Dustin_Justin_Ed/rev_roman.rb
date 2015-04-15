52f495c2f914de0c5364895f371fb02c97037192
Dustin Ashley <dashley@virtualhold.com>
2015-04-15 09:38:37 -0400

    Created tests for all numbers 1-10 as well as 40, 50, 90, 100, 400, 500, 900, 1000, etc. I also added functionality to take roman numeral string and return an integer by summing up each individual character's value. This works for all cases except numbers such as 4, 9, 40, etc because this requires additional logic.

--- Dustin_Justin_Ed/rev_roman.rb
+++ Dustin_Justin_Ed/rev_roman.rb
@@ -1,8 +1,69 @@
 class RomanNumeral
+
+  # I = 1
+  # V = 5
+  # X = 10
+  # L = 50
+  # C = 100
+  # D = 500
+  # M = 1000
+
   attr_reader :roman_numeral
+  attr_reader :roman_map
 
   def initialize
-    roman_numeral = String.new;
+    @roman_map = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    @subsequent_map = ['IV', 'VX', 'XL', 'LC', 'CD', 'DM']
+    @roman_numeral = String.new
+  end
+
+  def set_roman_numeral(val)
+    @roman_numeral = val
+  end
+
+  def roman_to_digit
+    validate_roman_numeral
+    handle_subsequent_chars
+    digit_value = 0
+    @roman_numeral.each_char do |char|
+      digit_value = digit_value + single_roman_to_digit(char)
+    end
+    digit_value
+  end
+
+  def handle_subsequent_chars
+    @subsequent_map.each do |item|
+    if @roman_numeral.index(item) != nil
+      puts @roman_numeral[@roman_numeral.index(item), 2]
+      puts @roman_numeral
+    end
+  end
+
+  end
+
+  def single_roman_to_digit(roman_char)
+    validate_roman_numeral
+    @roman_map.each_index do |i|
+      if roman_char == roman_map[i]
+        return (i % 2 == 0 ? 1 : 5) * (10 ** (i / 2))
+      end
+    end
+  end
+
+  def validate_roman_numeral
+    @roman_numeral.each_char do |char|
+      case char
+        when 'I'
+        when 'V'
+        when 'X'
+        when 'L'
+        when 'C'
+        when 'D'
+        when 'M'
+        else
+          raise IOError, "Invalid Roman Numeral"
+      end
+    end
   end
 
 end
--- Dustin_Justin_Ed/spec.rb
+++ Dustin_Justin_Ed/spec.rb
@@ -1,10 +1,119 @@
-require_relative 'rev_roman'
 require 'rspec'
+require_relative 'rev_roman'
 
-RSpec.describe RomanNumeral do
+# I = 1
+# V = 5
+# X = 10
+# L = 50
+# C = 100
+# D = 500
+# M = 1000
 
-  it 'will initialize as a String object' do
-    expect(subject.class).to eq(String)
+describe RomanNumeral do
+
+  it 'will initialize a RomanNumeral object with a string class attribute' do
+    expect(subject.class).to eq(RomanNumeral)
+    expect(subject.roman_numeral.class).to eq(String)
+  end
+
+  it 'will raise an IOError if invalid roman numeral is set incorrectly' do
+    subject.set_roman_numeral('Fail')
+    expect{subject.roman_to_digit}.to raise_error(IOError)
+  end
+
+  it 'will return "1" when the roman numeral is set to "I"' do
+    subject.set_roman_numeral('I')
+    expect(subject.roman_to_digit).to eq(1)
+  end
+
+  it 'will return "2" when the roman numeral is set to "II"' do
+    subject.set_roman_numeral('II')
+    expect(subject.roman_to_digit).to eq(2)
+  end
+
+  it 'will return "3" when the roman numeral is set to "III"' do
+    subject.set_roman_numeral('III')
+    expect(subject.roman_to_digit).to eq(3)
+  end
+
+  it 'will return "4" when the roman numeral is set to "IV"' do
+    subject.set_roman_numeral('IV')
+    expect(subject.roman_to_digit).to eq(4)
+  end
+
+  it 'will return "5" when the roman numeral is set to "V"' do
+    subject.set_roman_numeral('V')
+    expect(subject.roman_to_digit).to eq(5)
+  end
+
+  it 'will return "6" when the roman numeral is set to "VI"' do
+    subject.set_roman_numeral('VI')
+    expect(subject.roman_to_digit).to eq(6)
+  end
+
+  it 'will return "7" when the roman numeral is set to "VII"' do
+    subject.set_roman_numeral('VII')
+    expect(subject.roman_to_digit).to eq(7)
+  end
+
+  it 'will return "8" when the roman numeral is set to "VIII"' do
+    subject.set_roman_numeral('VIII')
+    expect(subject.roman_to_digit).to eq(8)
+  end
+
+  it 'will return "9" when the roman numeral is set to "IX"' do
+    subject.set_roman_numeral('IX')
+    expect(subject.roman_to_digit).to eq(9)
+  end
+
+  it 'will return "10" when the roman numeral is set to "X"' do
+    subject.set_roman_numeral('X')
+    expect(subject.roman_to_digit).to eq(10)
+  end
+
+  it 'will return "40" when the roman numeral is set to "XL"' do
+    subject.set_roman_numeral('X')
+    expect(subject.roman_to_digit).to eq(40)
+  end
+
+  it 'will return "50" when the roman numeral is set to "L"' do
+    subject.set_roman_numeral('L')
+    expect(subject.roman_to_digit).to eq(50)
+  end
+
+  it 'will return "90" when the roman numeral is set to "LC"' do
+    subject.set_roman_numeral('LC')
+    expect(subject.roman_to_digit).to eq(90)
+  end
+
+  it 'will return "100" when the roman numeral is set to "C"' do
+    subject.set_roman_numeral('C')
+    expect(subject.roman_to_digit).to eq(100)
+  end
+
+  it 'will return "400" when the roman numeral is set to "CD"' do
+    subject.set_roman_numeral('CD')
+    expect(subject.roman_to_digit).to eq(400)
+  end
+
+  it 'will return "500" when the roman numeral is set to "D"' do
+    subject.set_roman_numeral('D')
+    expect(subject.roman_to_digit).to eq(500)
+  end
+
+  it 'will return "900" when the roman numeral is set to "DM"' do
+    subject.set_roman_numeral('DM')
+    expect(subject.roman_to_digit).to eq(900)
+  end
+
+  it 'will return "1000" when the roman numeral is set to "M"' do
+    subject.set_roman_numeral('M')
+    expect(subject.roman_to_digit).to eq(1000)
+  end
+
+  it 'will return "1990" when the roman numeral is set to "MCMXC"' do
+    subject.set_roman_numeral('MCMXC')
+    expect(subject.roman_to_digit).to eq(1990)
   end
 
 end
